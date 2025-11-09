b96ccc18254fe084e5e31475e50861b2c29de02d
aadriien <adriienlynch@gmail.com>
2025-11-09 14:46:48 -0500

    Modified Svelte packaging to prep for GitHub Pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,21 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
   preprocess: vitePreprocess(),
-}
+
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    }),
+
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/cryptic-crosswords' : ''
+    }
+  }
+};
+
+export default config;
