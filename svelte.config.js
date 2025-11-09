bd905a3882773b6505bf09b0d68969bcc03a92dd
aadriien <adriienlynch@gmail.com>
2025-11-09 14:54:25 -0500

    Restored settings after realizing repo isn't using SvelteKit

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,7 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  preprocess: vitePreprocess(),
-
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: null
-    }),
-
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/cryptic-crosswords' : ''
-    }
-  }
-};
-
-export default config;
+export default defineConfig({
+  plugins: [svelte()],
+  base: '/cryptic-crosswords/'
+});
